5b3a5089106252b16b4395c47fd8e73397d408dc
Feras Elkharrat <feras.nizar2003@gmail.com>
2024-05-04 23:37:25 +0300

    skeleton now ready

--- Backend/models/passModle.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const passSchema = new mongoose.Schema({
-    Website : {type: String, required: true},
-    Username : {type: String, required: true},
-    Password : {type: String, required: true},
-});
-module.exports = mongoose.model('Password', passSchema);
-module.exports.Schema = passSchema;
