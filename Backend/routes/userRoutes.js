e1d6b8decbfca324edb9988a5b993dfc25dd50ee
Zaid Rass <zaidqarxoy@gmail.com>
2024-05-06 01:28:55 +0300

    Me final

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -8,4 +8,9 @@ router.get('/authenticate', authenticate, userController.authenticate);
 // Get profile by ID
 router.get('/profile', authenticate, userController.getProfile);
 
+
+router.put('/addNewPassword',authenticate, userController.addNewPassword);
+router.put('/decrypt',authenticate, userController.decrypt)
+router.delete('/deletePlatformPassword/:passwordId', authenticate, userController.deletePlatformPassword);
+router.put('/updateCredentials/:passwordId', authenticate, userController.updateCredentials);
 module.exports = router;
